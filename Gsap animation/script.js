b94833ed1c8ce50c66e60c6c613b496e9bf99717
krishna rajendra bhandari <bhandarikrishna057@gmail.com>
2021-12-31 10:56:50 +0530

    Gsap animation

---
+++ Gsap animation/script.js
@@ -0,0 +1,31 @@
+gsap.to(".item1", {
+  duration: 1,
+  x: "-100%",
+  delay: "1",
+  ease: "expo.inOut",
+});
+gsap.to(".item2", {
+  duration: 1,
+  x: "-100%",
+  delay: "1.3",
+  ease: "expo.inOut",
+});
+gsap.to(".item3", {
+  duration: 1,
+  x: "-100%",
+  delay: "1.6",
+  ease: "expo.inOut",
+});
+gsap.to(".item4", {
+  duration: 1,
+  x: "-100%",
+  delay: "1.9",
+  ease: "expo.inOut",
+});
+
+// const timeline = gsap.timeline({ default: { duration: 1 } });
+
+// timeline.to(".item1", { x: "-100%", ease: "expo.out" });
+// timeline.to(".item2", { x: "-100%", ease: "expo.out" });
+// timeline.to(".item3", { x: "-100%", ease: "expo.out" });
+// timeline.to(".item4", { x: "-100%", ease: "expo.out" });
